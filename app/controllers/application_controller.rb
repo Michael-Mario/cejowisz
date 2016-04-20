4eaad8430c6961ac7f994308da6564cf064ba0ff
Justinobi <cjustinobi@gmail.com>
2016-04-20 10:58:24 +0000

    finsihed static branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  
   def hello
     render text: "I'm hacked by an amazing girlfriend"
   end
